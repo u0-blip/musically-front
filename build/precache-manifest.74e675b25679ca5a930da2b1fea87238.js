dd00ccbeeeedf471782f8e696384d77d6ef7facb
Peter <chenduona@gmail.com>
2021-05-03 19:58:57 +1000

    using static build because netlify is being dodgy af

---
+++ build/precache-manifest.74e675b25679ca5a930da2b1fea87238.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4b0176e22c4c141ca0d644d84a7527a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efa1aafdc4aeec0a4eda",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "76875aa0f88217170132",
+    "url": "/static/js/2.853ed804.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/static/js/2.853ed804.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efa1aafdc4aeec0a4eda",
+    "url": "/static/js/main.13a18fdc.chunk.js"
+  },
+  {
+    "revision": "1b010999b20cce9f45de",
+    "url": "/static/js/runtime-main.6396098f.js"
+  }
+]);
